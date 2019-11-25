4734d9c582d6e2aedca08f1a0ac5b5edbe6c3e84
Gabriel Victorino <55957207+gabrielvictorino1997@users.noreply.github.com>
2019-11-25 18:04:40 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class ThreadTest
+    {
+        private string name;
+        public ThreadTest(String name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get=> name ; set => name = value; }
+
+        public void ThreadRun()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(name + " - " + i);
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadTest test = new ThreadTest("Thread " + i);
+                Thread thread = new Thread(new ThreadStart(test.ThreadRun));
+                thread.Start();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
